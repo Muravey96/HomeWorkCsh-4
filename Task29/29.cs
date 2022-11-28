520623ca1609cc0e53f1580710a5d473853bff9f
DariaMuravitskaya <muraviczkaya96@gmail.com>
2022-11-28 22:31:46 +0300

    solved task 29

--- Task29/29.cs
+++ Task29/29.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+Console.Clear();
+int length = 8;
+int[] array = new int[length];
+
+FillArrayN(array);
+PrintArray(array);
+
+/////////////
+
+void FillArrayN(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-20, 20);
+    }
+
+}
+
+/////////////
+
+void PrintArray(int[] array)
+{
+    Console.Write($"[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write($"]");
+}
+
