25995ecdd2da747d395ce341e0e71159ba45e312
DariaMuravitskaya <muraviczkaya96@gmail.com>
2022-11-28 20:46:50 +0300

    solved task 27

--- Task27/27.cs
+++ Task27/27.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+ Console.Clear();
+int userNum = GetNumberFromUser("Введите целое число: ","Ошибка ввода!");
+int sum = GetSumForNumber(userNum);
+Console.WriteLine($"{userNum} -> {sum}");
+
+
+
+
+//////////
+int GetNumberFromUser(string message, string errorMessage)
+
+{
+    while(true)
+    {
+        Console.WriteLine(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNum);
+        if (isCorrect)  return userNum;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+/////////
+int GetSumForNumber (int number)
+{
+    int counter = Convert.ToString(number).Length;
+    int middleres = 0;
+    int result = 0;
+
+    for (int i=0; i<counter; i++ )
+    {
+      middleres = number - number % 10;
+      result = result + (number - middleres);
+      number = number / 10;
+    }
+   return result;
+}
+
